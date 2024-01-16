a5d261e933dc2973b7e49d9470ae741c46a7a443
Milaadmg023 <132762516+Milaadmg023@users.noreply.github.com>
2024-01-16 18:44:12 +0330

    project created

---
+++ app.js
@@ -0,0 +1,42 @@
+let menu = document.querySelector(".sidebar__menu");
+
+function menuHandler() {
+  menu.classList.toggle("hidden");
+}
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  loop: true,
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".popular__products", {
+     slidesPerView: 5,
+     spaceBetween: 30,
+     loop: true,
+     autoplay: {
+       delay: 2500,
+       disableOnInteraction: false,
+     },
+     navigation: {
+       nextEl: ".swiper-button-next",
+       prevEl: ".swiper-button-prev",
+     },
+   });
